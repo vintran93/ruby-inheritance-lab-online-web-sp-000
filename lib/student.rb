74fb396029da5ffd985f9210c3c7cbd7bb546f7f
Vincent Tran <vincentuong@yahoo.com>
2021-06-21 03:28:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,11 @@ class Student < User
   attr_reader :first_name, :last_name
 
 
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
-    @knowledge << self
+    @knowledge = []
   end
 
 end
